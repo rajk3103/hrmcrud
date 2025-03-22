c3118b97d9253f589d4064f27310476d95fcc642
rajk3103 <raje3103@gmail.com>
2025-03-22 17:22:37 +0530

    initial commit

---
+++ src/main/java/com/fullsatck/HrmcrudApplication.java
@@ -0,0 +1,13 @@
+package com.fullsatck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrmcrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrmcrudApplication.class, args);
+	}
+
+}
